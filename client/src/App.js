2e3ef41e7141a0357cb1a334c6ae66ce957bfb97
anand-yadav55 <anand55yadav@gmail.com>
2021-09-02 12:47:25 +0530

    Auth components

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,15 @@
+import React from 'react';
+import Login from './components/auth/Login';
+import Signup from './components/auth/Signup';
+import Dashboard from './components/Dashboard';
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Dashboard />
+      <Login />
+      <Signup />
+    </div>
+  );
 }
 
 export default App;
