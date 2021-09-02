c00632b9133787c209b15d9b6d69caea69db37ef
anand-yadav55 <annad55yadav@gmail.com>
2021-09-02 16:27:16 +0530

    folder structure

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,15 @@ import React from 'react';
 import Login from './components/auth/Login';
 import Signup from './components/auth/Signup';
 import Dashboard from './components/Dashboard';
+import {BrowserRouter, Route} from 'react-router-dom';
 function App() {
   return (
     <div className="App">
-      <Dashboard />
-      <Login />
-      <Signup />
+      <BrowserRouter>
+      <Route path='/Login' component={Login}/>
+      <Route path='/Signup' component={Signup}/>
+      <Route path='/Dashboard' component={Dashboard}/>
+      </BrowserRouter>
     </div>
   );
 }
